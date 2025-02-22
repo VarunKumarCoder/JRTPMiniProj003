ed9a6393fc2c5109da35babbbab1540d1b4aea46
VarunKumarCoder <medavarunkumar@gmail.com>
2025-02-22 23:45:04 +0530

    Created Basic Project structure,Packages and Database and JPA and server port details in application.properties file.

---
+++ JRTPMiniProj003UserMgmt/src/main/java/com/nt/JrtpMiniProj003UserMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrtpMiniProj003UserMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrtpMiniProj003UserMgmtApplication.class, args);
+	}
+
+}
