67c88de62b5d7b6bf0b9f2798d35a69e858a9002
VarunKumarCoder <medavarunkumar@gmail.com>
2025-02-23 15:52:21 +0530

    Recover Password page Model Class is completed

--- JRTPMiniProj003UserMgmt/src/main/java/com/nt/bindings/RecoverPassword.java
+++ JRTPMiniProj003UserMgmt/src/main/java/com/nt/bindings/RecoverPassword.java
@@ -5,10 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class LoginCredentials {
+@AllArgsConstructor
+public class RecoverPassword {
 
 	private String name;
-	private String password;
+	private String email;
 }
