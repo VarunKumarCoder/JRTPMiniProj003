2e44b7dc003e82fa560c87476e1a6c9409f5d99f
VarunKumarCoder <medavarunkumar@gmail.com>
2025-02-23 15:35:59 +0530

    User Account Model class done

---
+++ JRTPMiniProj003UserMgmt/src/main/java/com/nt/bindings/ActivateUser.java
@@ -0,0 +1,15 @@
+package com.nt.bindings;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActivateUser {
+	
+	private 
+}
