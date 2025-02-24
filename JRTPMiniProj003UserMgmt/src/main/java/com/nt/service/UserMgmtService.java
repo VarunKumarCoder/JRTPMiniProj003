0632b283a0a803975af07f8d8d6534f22ddc1240
VarunKumarCoder <medavarunkumar@gmail.com>
2025-02-24 11:10:03 +0530

    Service interface methods are completed to save the userDetails of all registration pages

---
+++ JRTPMiniProj003UserMgmt/src/main/java/com/nt/service/UserMgmtService.java
@@ -0,0 +1,23 @@
+package com.nt.service;
+
+import java.util.List;
+
+import com.nt.bindings.ActivateUser;
+import com.nt.bindings.LoginCredentials;
+import com.nt.bindings.RecoverPassword;
+import com.nt.bindings.UserAccount;
+
+public interface UserMgmtService {
+	
+	public String registerUser(UserAccount user);
+	public String activateUserAccount(ActivateUser user);
+	public String login(LoginCredentials credentials);
+	public String recoverPassword(RecoverPassword recover);
+	public List<UserAccount> listUsers();
+	public UserAccount showUsersById(Integer id);
+	public UserAccount showUsersByEmailAndName(String email,String name);
+	public String updateUser(UserAccount user);
+	public String deleteUserById(Integer id);
+	public String changeUserStatus(Integer id,String status);
+	
+}
